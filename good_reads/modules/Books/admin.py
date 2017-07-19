f4bdc1f7d09bf609c5d80877533c15a6f7b6ed59
joseaznar <jose.aznar94@gmail.com>
2017-07-18 19:39:45 -0500

    Register the models

--- good_reads/modules/Books/admin.py
+++ good_reads/modules/Books/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Book, Coments
 
 # Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+class ComentsAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Coments, ComentsAdmin)
+admin.site.register(Book, BookAdmin)
--- good_reads/modules/Users/admin.py
+++ good_reads/modules/Users/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as UA
 
-# Register your models here.
+from  .models import User
+
+
+class UserAdmin(UA):
+
+    fieldsets = (
+        ('Account', {
+            'fields': ('email', 'password', 'is_active', 'is_staff')
+        }), ('Personal Data', {
+            'fields': ('nombre', 'last_name','celphone','gender','library')
+
+        })
+
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+    def username(self, instance):
+        return instance.email
+
+    def user_first_name (self,instance):
+        return instance.nombre
+
+    def user_last_name (self,instance):
+        return instance.last_name
+
+    list_display = ('email', 'user_first_name', 'user_last_name','is_active')
+    ordering = ('email',)
+
+admin.site.register(User, UserAdmin)
